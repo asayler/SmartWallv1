c12a3995c2c326ffca81229a2c0c11d535f2eef2
asayler <neueWelt@gmail.com>
2011-04-10 20:33:05 -0400

    Removed endian debug output and added pic32 outlet burn-in test script

---
+++ usermon/outletTest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Andy Sayler
+# Send continour messages to outlet
+
+while [ 1 ]; do
+    ./swChnMsg 0x0012 QUERY OUTLET 0x0010 4 0 x 1 x
+    sleep .25
+    ./swChnMsg 0x0012 SET OUTLET 0x0010 4 0 1 1 1
+    sleep .25
+    ./swChnMsg 0x0012 QUERY OUTLET 0x0010 4 0 x 1 x
+    sleep .25
+    ./swChnMsg 0x0012 SET OUTLET 0x0010 4 0 0 1 0
+    sleep .25
+    ./swChnMsg 0x0012 QUERY OUTLET 0x0010 4 0 x
+    sleep .25
+    ./swChnMsg 0x0012 SET OUTLET 0x0010 4 0 1
+    sleep .25
+    ./swChnMsg 0x0012 QUERY OUTLET 0x0010 4 1 x
+    sleep .25
+    ./swChnMsg 0x0012 SET OUTLET 0x0010 4 1 1
+    sleep .25
+    ./swChnMsg 0x0012 QUERY OUTLET 0x0010 4 0 x
+    sleep .25
+    ./swChnMsg 0x0012 SET OUTLET 0x0010 4 0 0
+    sleep .25
+    ./swChnMsg 0x0012 QUERY OUTLET 0x0010 4 1 x
+    sleep .25
+    ./swChnMsg 0x0012 SET OUTLET 0x0010 4 1 0
+    sleep .25
+done
